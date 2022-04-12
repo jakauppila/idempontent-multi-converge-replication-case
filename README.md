88a6e7559d8314c937a58b3e4ea0ba4c86c7f252
Jared Kauppila <Jared.Kauppila@sentry.com>
2022-04-12 14:14:33 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# multiple-converge
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: multiple-converge
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
